67d40e6fbfbed3985c3dca1acf73c2000a13de35
Jesse S <jelloeater@gmail.com>
2016-03-13 23:35:27 -0400

    Added debug logging, arg parser and main method

--- run.py
+++ run.py
@@ -1,42 +1,56 @@
+import argparse
 from datetime import datetime
+import logging
 import os
+import speedtest_cli
+
+print(speedtest_cli.__file__)
 
 __author__ = 'jesse'
 
-def detectOS():
-    from sys import platform as _platform
-    if _platform == "linux" or _platform == "linux2":
-        pass
-    # linux
-    elif _platform == "darwin":
-        pass
-    # MAC OS X
-    elif _platform == "win32":
-        pass
-    # Windows
+
+def main():
+    LOG_FILENAME = 'error.log'
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--debug",
+                        action="store_true",
+                        help="Debug Mode Logging")
+    args = parser.parse_args()
+    if args.debug:
+        logging.basicConfig(format="[%(asctime)s] [%(levelname)8s] --- %(message)s (%(filename)s:%(lineno)s)",
+                            level=logging.DEBUG)
+        logging.debug('Debug Mode Enabled')
+    else:
+        logging.basicConfig(filename=LOG_FILENAME,
+                            format="[%(asctime)s] [%(levelname)8s] --- %(message)s (%(filename)s:%(lineno)s)",
+                            level=logging.WARNING)
+
+    data = SpeedTest().doSpeedTest()
+
+    # TODO Save data to SQLlite
+    # TODO Create DB w/ sqlalchemy
+
+    # TODO Email Report
+    # TODO Pretty text table module for presentation
+
 
 class SpeedTest():
     def doSpeedTest(self):
-            result = os.popen("/usr/local/bin/speedtest-cli --simple").read()
-            # TODO Add switch for different OS versions
+        logging.debug(speedtest_cli.__file__)
+        result = os.popen("/usr/local/bin/speedtest-cli --simple").read()
 
-            if 'Cannot' in result:
-                return { 'date': datetime.now(), 'uploadResult': 0, 'downloadResult': 0, 'ping': 0 }
+        if 'Cannot' in result:
+            return {'date': datetime.now(), 'uploadResult': 0, 'downloadResult': 0, 'ping': 0}
 
-            resultSet = result.split('\n')
-            pingResult = resultSet[0]
-            downloadResult = resultSet[1]
-            uploadResult = resultSet[2]
+        resultSet = result.split('\n')
+        pingResult = resultSet[0]
+        downloadResult = resultSet[1]
+        uploadResult = resultSet[2]
 
-            self.pingResult = float(pingResult.replace('Ping: ', '').replace(' ms', ''))
-            self.downloadResult = float(downloadResult.replace('Download: ', '').replace(' Mbit/s', ''))
-            self.uploadResult = float(uploadResult.replace('Upload: ', '').replace(' Mbit/s', ''))
-            return self
+        self.pingResult = float(pingResult.replace('Ping: ', '').replace(' ms', ''))
+        self.downloadResult = float(downloadResult.replace('Download: ', '').replace(' Mbit/s', ''))
+        self.uploadResult = float(uploadResult.replace('Upload: ', '').replace(' Mbit/s', ''))
+        return self
 
-data=SpeedTest().doSpeedTest()
-
-#TODO Save data to SQLlite
-#TODO Create DB w/ sqlalchemy
-
-#TODO Email Report
-#TODO Pretty text table module for presentation
+if __name__ == "__main__":
+    main()
