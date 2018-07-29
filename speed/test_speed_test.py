d69a84f30a1b3fe7112ece3c6f76d3976364bfa0
Jesse <jelloeater@gmail.com>
2018-07-29 14:07:28 -0400

    Started working on unit testing

--- speed/test_speed_test.py
+++ speed/test_speed_test.py
@@ -1,8 +1,14 @@
+import logging
+logging.basicConfig(format="[%(asctime)s] [%(levelname)8s] --- %(message)s "
+                           "(%(filename)s:%(lineno)s)",
+                    level=logging.DEBUG)
 from unittest import TestCase
-
 import speed_test
 
 
 class TestGetSpeedTest(TestCase):
     def test_doSpeedTest(self):
+        logging.debug('Starting SpeedTest Unit Test')
         assert speed_test.GetSpeedTest().doSpeedTest()
+
+TestGetSpeedTest().test_doSpeedTest()
