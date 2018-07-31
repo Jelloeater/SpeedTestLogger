d5184e2b81208cb4810015568dfe4118a7075c25
Jesse <jelloeater@gmail.com>
2018-07-31 00:40:52 -0400

    Added requirements Still need to get py.test working

--- speed/test_speed_test.py
+++ speed/test_speed_test.py
@@ -2,13 +2,10 @@ import logging
 logging.basicConfig(format="[%(asctime)s] [%(levelname)8s] --- %(message)s "
                            "(%(filename)s:%(lineno)s)",
                     level=logging.DEBUG)
-from unittest import TestCase
 import speed_test
 
 
-class TestGetSpeedTest(TestCase):
+class TestGetSpeedTest():
     def test_doSpeedTest(self):
         logging.debug('Starting SpeedTest Unit Test')
         assert speed_test.GetSpeedTest().doSpeedTest()
-
-TestGetSpeedTest().test_doSpeedTest()
