a44c8e6ced5f2a2e3fa98d85d658031cd1af0a51
Jesse <jelloeater@gmail.com>
2018-08-05 01:46:30 -0400

    Testing debug ssh

--- speed/test_speed_test.py
+++ speed/test_speed_test.py
@@ -1,11 +1,16 @@
 import logging
+import pytest
 logging.basicConfig(format="[%(asctime)s] [%(levelname)8s] --- %(message)s "
                            "(%(filename)s:%(lineno)s)",
                     level=logging.DEBUG)
+logging.debug('TEST START')
 import speed_test
 
 
 class TestGetSpeedTest():
+    def __init__(self):
+        pass
+
     def test_doSpeedTest(self):
         logging.debug('Starting SpeedTest Unit Test')
         assert speed_test.GetSpeedTest().doSpeedTest()
